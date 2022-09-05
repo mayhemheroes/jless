cd49b7a964d03f51b6e213d5cf6e5286d0d4e594
Evan Richter <evanjrichter@gmail.com>
2022-09-05 16:34:25 -0500

    json parser fuzzing

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#![cfg(fuzzing)]
+mod app;
+mod flatjson;
+mod highlighting;
+mod input;
+pub mod jsonparser;
+mod jsontokenizer;
+mod lineprinter;
+mod options;
+mod screenwriter;
+mod search;
+mod terminal;
+mod truncatedstrview;
+mod types;
+mod viewer;
+mod yamlparser;
